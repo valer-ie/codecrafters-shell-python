e191a526ba0d1c0de1c068000fced148ed152d86
v-lerie <valerie18dsouza@gmail.com>
2025-05-07 00:08:51 -0400

    Step 4: The exit builtin

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ def main():
 
     # Wait for user input
         command = input()
-        if command() == "exit 0":
+        if command == 0:
             sys.exit(0)
 
         print(f"{command}: command not found")
